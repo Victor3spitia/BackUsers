9013858364082081bcb89bc157518b0b16ee871a
Victor3spitia <149959687+Victor3spitia@users.noreply.github.com>
2025-06-16 19:09:42 -0500

    Delete src directory

--- src/auth/authConstants.ts
+++
@@ -1,3 +0,0 @@
-export const API_URL = "http://localhost:3000/api";
-//cuidado con cambiar la ruta raiz
-//para unificar la URL
--- src/routes/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-import { useAuth } from "../auth/AuthProvider";
-
-export default function ProtectedRoute() {
-  const auth = useAuth();
-
-  return auth.isAuthenticated ? <Outlet /> : <Navigate to="/" />;
-} // si la variable outlet es verdadera muestre lo que hay debajo de protectedRoute en el main
